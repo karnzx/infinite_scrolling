5c2f19147afc2fa050d2f5f4d27bffc580e1c296
karnzx <karnzx-18762@hotmail.com>
2021-07-12 19:34:37 +0700

    modified

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:infinite_scrolling/starwars_repo.dart';
+import 'dart:developer';
 
 void main() {
   runApp(MyApp());
@@ -8,12 +10,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    print(StarwarsRepo().fetchPeople());
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Text('hello'),
     );
   }
 }
